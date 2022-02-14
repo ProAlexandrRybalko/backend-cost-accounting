361009bfc3db5ae1998f218fef17178ce5dad104
ProAlexandrRybalko <rybalko.exceedteam@gmail.com>
2022-02-14 12:12:57 +0300

    HotFix/added data key, query is replaced by body

--- src/modules/controllers/accounting.controller.js
+++ src/modules/controllers/accounting.controller.js
@@ -10,18 +10,18 @@ module.exports.getAllAccountings = (req, res) => {
 module.exports.createNewAccounting = (req, res) => {
   const accounting = new Accounting(req.body);
   accounting.save().then(result => {
-    res.send(result);
+    res.send({data: result});
   }).catch(err => console.log(err));
 }
 
 module.exports.deleteAccounting = (req, res) => {
-  Accounting.deleteOne({ _id: req.query.id}).then(result => {
+  Accounting.deleteOne({ _id: req.body.id}).then(result => {
     res.send({data: "deleted"});
   }).catch(err => console.log(err));
 }
 
 module.exports.changeAccounting = (req, res) => {
-  Accounting.updateOne({ _id: req.body.id}, req.body).then(result => {
+  Accounting.updateOne({ _id: req.body.id}, req.body.data).then(result => {
     res.send({data: "changed"});
   }).catch(err => console.log(err));
 }
